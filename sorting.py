080e2fc2d59033fd96f37f722dd589520dd4712b
Pham Gia Khang <khangpham1052006@gmail.com>
2026-01-18 11:24:30 +0700

    Merge_sort

--- sorting.py
+++ sorting.py
@@ -80,11 +80,12 @@ def partition(arr, low, high):
     pivot=arr[pivotIndex]
     arr[pivotIndex], arr[high] = arr[high], arr[pivotIndex]
     i=low
-    for j in range(low, high-1):
+    for j in range(low, high):
         if (arr[j]<=pivot):
             arr[i], arr[j]=arr[j], arr[i]
             i+=1
     arr[i], arr[high]= arr[high], arr[i]
+    return i #vị trí hiện tại của i chính là vị trí của pivot, mà ta cần chia làm 2 tiếp
 
 def quick_sort(arr,low, high):
     if low<high:
@@ -92,10 +93,54 @@ def quick_sort(arr,low, high):
 
         quick_sort(arr, low, pivot_index-1)
         quick_sort(arr, pivot_index+1, high)
+    else: 
+        return 
 
 arr5 = [random.randint(1, 100) for _ in range(10)]
 print()
 print("Quick sort test:")
 print(arr5)
-insertion_sort(arr5)
+quick_sort(arr5, 0, 9)
 print(arr5)
+
+def merge_sort(arr, lo, hi):
+    if lo>=hi:
+        return
+    mid=lo+ (hi-lo)//2
+    merge_sort(arr, lo, mid)
+    merge_sort(arr, mid+1, hi)
+    merge(arr, lo, mid, hi)
+
+
+def merge(arr, lo, mid, hi):
+    left_arr=arr[lo: mid+1]
+    right_arr=arr[mid+1: hi]
+    i=0 
+    j=0
+    k=lo
+    while (i<len(left_arr) and j<len(right_arr)):
+        if (left_arr[i] < right_arr[j]):
+            arr[k]=left_arr[i]
+            i+=1
+        
+        else :
+            arr[k]=right_arr[j]
+            j+=1
+        k+=1
+    while (i<len(left_arr)):
+        arr[k]=left_arr[i]
+        k+=1
+        i+=1
+    
+    while (j<len(right_arr)):
+        arr[k]=right_arr[j]
+        k+=1
+        j+=1
+    
+
+arr6 = [random.randint(1, 100) for _ in range(10)]
+print()
+print("Merge sort test:")
+print(arr6)
+merge_sort(arr6, 0 ,len(arr6)-1)
+print(arr6)
